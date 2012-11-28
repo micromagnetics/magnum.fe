cdf0894c96e995b57e8a12e6874f1b915cb1db70
Claas Abert <claas@cabird.de>
2012-11-28 19:54:36 +0100

    Best version ever

--- tests/integration/llg_test.py
+++ tests/integration/llg_test.py
@@ -5,8 +5,26 @@ import numpy
 
 class LlgTest(unittest.TestCase):
 
+  def ttest_dm(self):
+    mesh  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=3) #, scale=1e-9)
+    VV    = VectorFunctionSpace(mesh, "CG", 1, 3)
+
+    arg = "sqrt((3.141592*(x[0]/1e-6))*(3.141592*(x[0]/1e-6)))"
+    m   = interpolate(Expression(("cos(%s)" % arg, "sin(%s)" % arg, "0.0")), VV)
+
+    material = Material.py()
+    #material.alpha = 1.0
+
+    llg = LLG(mesh, material, scale=1e-9)
+    dm = llg.calculate_dm(m, 1e-9)
+
+    f = File("data/m.pvd")
+    f << m
+    f = File("data/dm.pvd")
+    f << dm
+
   def prepare_s_state(self):
-    mesh  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=3, scale=1e-9)
+    mesh  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=3) #, scale=1e-9)
     VV    = VectorFunctionSpace(mesh, "CG", 1, 3)
 
     filename = "data/s-state.xml"
@@ -16,17 +34,19 @@ class LlgTest(unittest.TestCase):
       return Function(VV, filename)
 
     except IOError as e:
-      # File does not exists, calculate s-state and return
-      arg = "sqrt((3.141592*(x[0]/1e-6))*(3.141592*(x[0]/1e-6)))"
-      m   = interpolate(Expression(("cos(%s)" % arg, "sin(%s)" % arg, "0.0")), VV)
-
       material = Material.py()
       material.alpha = 1.0
+      llg = LLG2(mesh, material, scale=1e-9)
 
-      llg = LLG2(mesh, material)
+      # File does not exists, calculate s-state and return
+      arg = "sqrt((3.141592*(x[0]/1e3))*(3.141592*(x[0]/1e3)))"
+      m   = llg.interpolate(Expression(("cos(%s)" % arg, "sin(%s)" % arg, "0.0")))
+
 
       for i in range(60):
-        m = llg.step(m, 8e-12)
+        m = llg.step(m, 1e-11)
+        #f = File("data/m_%d.pvd" % i)
+        #f << m
 
       f = File(filename)
       f << m
@@ -34,15 +54,16 @@ class LlgTest(unittest.TestCase):
       return m
 
   def test_sp4(self):
-    mesh  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=3, scale=1e-9)
+    mesh  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=3) #, scale=1e-9)
     VV    = VectorFunctionSpace(mesh, "CG", 1, 3)
 
     m = self.prepare_s_state()
-    llg = LLG2(mesh, Material.py())
+
+    llg = LLG2(mesh, Material.py(), scale=1e-9)
     field = Constant((-24.6e-3/Constants.mu0, +4.3e-3/Constants.mu0, 0.0))
 
     scalar_file = open("data/sp4.dat","w",0)
-    dt = 5e-14
+    dt = 5e-13
     T  = 1e-9
 
     t  = 0.0
@@ -51,12 +72,13 @@ class LlgTest(unittest.TestCase):
       t = i * dt
       
       # write magnetization configuration (only each 20th step)
-      if (i % 20 == 0):
-        f = File("data/m_%d.pvd" % int(i/20))
+      nth_step = 10 
+      if (i % nth_step == 0):
+        f = File("data/m_%d.pvd" % int(i/nth_step))
         f << m
 
       # write scalar information
-      volume = 187500e-27 
+      volume = 187500
       m_x = assemble(m[0] / volume * dx)
       m_y = assemble(m[1] / volume * dx)
       m_z = assemble(m[2] / volume * dx)
