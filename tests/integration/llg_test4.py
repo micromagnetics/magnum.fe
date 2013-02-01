324d0e13c8bba087dd7462e7d2939d4c06dd3098
Claas Abert <claas@cabird.de>
2013-02-01 15:42:38 +0100

    first somehow converging version of llg4

--- tests/integration/llg_test4.py
+++ tests/integration/llg_test4.py
@@ -3,7 +3,8 @@ from dolfin import *
 from magnumfe import *
 import numpy
 
-mesh           = DemagField.create_mesh((50.0/2.0, 50.0/2.0, 3.0/2.0), (10, 10, 1), d=3)
+#mesh           = DemagField.create_mesh((50.0/2.0, 50.0/2.0, 3.0/2.0), (10, 10, 1), d=3)
+mesh           = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=4)
 VV             = VectorFunctionSpace(mesh, "CG", 1, 3)
 
 material       = Material.py()
