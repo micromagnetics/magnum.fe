5ddca9a16081a7f6f864f84fb3aacf9591e45eb5
Claas Abert <claas@cabird.de>
2014-04-24 17:01:36 +0200

    some fixes

--- tests/integration/llg_test.py
+++ tests/integration/llg_test.py
@@ -3,8 +3,8 @@ from dolfin import *
 from magnumfe import *
 import numpy
 
-mesh  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=4)
-VV    = VectorFunctionSpace(mesh, "CG", 1, 2)
+mesh, sample_size  = DemagField.create_mesh((500.0/2.0, 125.0/2.0, 3.0/2.0), (100, 25, 1), d=4)
+VV    = VectorFunctionSpace(mesh, "CG", 1, 3)
 
 class LlgTest(unittest.TestCase):
 
@@ -18,30 +18,34 @@ class LlgTest(unittest.TestCase):
     except IOError as e:
       material = Material.py()
       material.alpha = 1.0
-      llg = LLG(mesh, material, scale=1e-9, demag_order=2)
 
       # File does not exists, calculate s-state and return
       arg = "sqrt((3.141592*(x[0]/1e3))*(3.141592*(x[0]/1e3)))"
-      m   = llg.interpolate(Expression(("cos(%s)" % arg, "sin(%s)" % arg, "0.0")))
+      m_start = Expression(("cos(%s)" % arg, "sin(%s)" % arg, "0.0"))
 
+      state = State(mesh, material = material, m = m_start)
+      llg = LLG([DemagField(sample_size, 2)], scale = 1e-9)
 
       volume = 187500
       for i in range(400):
-        m = llg.step(m, 1e-11)
-        m_x = assemble(m[0] / volume * dx)
+        llg.step(state, 1e-11)
+        m_x = assemble(state.m[0] / volume * dx)
         print "Mx: %f" % m_x
 
 
       f = File(filename)
-      f << m
+      f << state.m
 
-      return m
+      return state.m
 
   def test_sp4(self):
-    m = self.prepare_s_state()
+    m_start = self.prepare_s_state()
+    state   = State(mesh, material = Material.py(), m = m_start)
 
-    llg = LLG(mesh, Material.py(), scale=1e-9, demag_order=2)
-    field = Constant((-24.6e-3/Constants.mu0, +4.3e-3/Constants.mu0, 0.0))
+    llg = LLG([
+        ExternalField((-24.6e-3/Constants.mu0, +4.3e-3/Constants.mu0, 0.0)),
+        DemagField(sample_size, 2)
+    ], scale = 1e-9)
 
     scalar_file = open("data/sp4.dat","w",0)
     dt = 1e-13
@@ -56,17 +60,17 @@ class LlgTest(unittest.TestCase):
       nth_step = 10 
       if (i % nth_step == 0):
         f = File("data/m_%d.pvd" % int(i/nth_step))
-        f << m
+        f << state.m
 
       # write scalar information
       volume = 187500
-      m_x = assemble(m[0] / volume * dx)
-      m_y = assemble(m[1] / volume * dx)
-      m_z = assemble(m[2] / volume * dx)
+      m_x = assemble(state.m[0] / volume * dx)
+      m_y = assemble(state.m[1] / volume * dx)
+      m_z = assemble(state.m[2] / volume * dx)
       scalar_file.write("%.10f %f %f %f\n" % (t*1e9, m_x, m_y, m_z))
 
       # calculate next step
-      m = llg.step(m, dt, h_ext = field)
+      llg.step(state, dt)
 
     scalar_file.close()
 
