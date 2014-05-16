348f86cc8d169b410839b7f6642bb608786e7042
Claas Abert <claas@cabird.de>
2014-05-16 12:42:36 +0200

    towards the first spin diffusion example

---
+++ examples/spin_diffusion/hysteresis.py
@@ -0,0 +1,88 @@
+from dolfin import *
+from magnumfe import *
+
+#######################################
+#### GENERATE MESH WITH SUBDOMAINS
+#######################################
+
+class FM1(SubDomain):
+  def inside(self, x, on_boundary):
+    return between(x[2], (-90, 10))
+
+class NM(SubDomain):
+  def inside(self, x, on_boundary):
+    return between(x[2], (10, 30))
+
+class FM2(SubDomain):
+  def inside(self, x, on_boundary):
+    return between(x[2], (30, 90))
+
+mesh, sample_size = DemagField.create_mesh(
+    (128.0/2.0, 64.0/2.0, 180/2.0), (30, 15, 36),
+    d = 4,
+    domains = { 1: FM1(), 2: NM(), 3: FM2()}
+  )
+
+#######################################
+#### DEFINE STATE AND MATERIAL
+#######################################
+
+state = State(mesh, {'magnetic': (1, 3), 'conducting': (1, 2, 3)},
+    m = Constant((0, 0, 1)),
+    s = Constant((0, 0, 0)),
+    j = Constant((0, 0, 1e11))
+  )
+
+state.material['magnetic']  = Material(
+  alpha      = 0.1,
+  ms         = 8e5,
+  Aex        = 13e-12,
+  K_uni      = 500,
+  K_uni_axis = (0, 0, 1),
+  D0         = 1e-3,
+)
+state.material['!magnetic']  = Material(
+  D0         = 5e-3
+)
+state.material['conducting'] = Material(
+  beta       = 0.9,
+  beta_prime = 0.8,
+  lambda_sf  = 10e-9,
+  lambda_j   = 4e-9,
+  c          = 3.125e-3 # J?
+)
+
+external_field = ExternalField((0.0, 0.0, 0.0))
+llg      = LLG([
+  DemagField(sample_size, 2),
+  #UniaxialAnisotropyField(),
+  SpinCurrent(),
+  external_field
+], scale = 1e-9)
+
+spindiff = SpinDiffusion()
+
+v_magnetic = Constant(assemble(Constant(1.0)*state.dx('magnetic')))
+logfile = open("hyst.dat", "w", 0)
+
+steps = 10
+for i in range(steps+1):
+  external_field.set((0.0, 0.0, (1.0 - 2.0*i/steps) * 60e-3/Constants.mu0))
+
+  # relax
+  for j in range(100):
+    llg.step(state, 1e-12)
+    spindiff.step(state, 1e-12)
+
+  m_x = assemble(state.m[0] / v_magnetic * state.dx('magnetic'))
+  m_y = assemble(state.m[1] / v_magnetic * state.dx('magnetic'))
+  m_z = assemble(state.m[2] / v_magnetic * state.dx('magnetic'))
+  logfile.write("%f %f %f\n" % (m_x, m_y, m_z))
+
+  f = File("data/m_%d.pvd" % int(i/10))
+  f << state.m
+
+  f = File("data/s_%d.pvd" % int(i/10))
+  f << state.s
+
+logfile.close()
