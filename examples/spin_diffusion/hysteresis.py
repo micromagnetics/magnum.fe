611016ee125e769549e6392c7bd4b5fc86ad08c2
Claas Abert <claas@cabird.de>
2014-05-21 16:18:17 +0200

    minor changes

--- examples/spin_diffusion/hysteresis.py
+++ examples/spin_diffusion/hysteresis.py
@@ -17,18 +17,25 @@ class FM2(SubDomain):
   def inside(self, x, on_boundary):
     return between(x[2], (30, 90))
 
-mesh, sample_size = DemagField.create_mesh(
-    (128.0/2.0, 64.0/2.0, 180/2.0), (30, 15, 36),
-    d = 4,
-    domains = { 1: FM1(), 2: NM(), 3: FM2()}
-  )
+#mesh, sample_size = DemagField.create_mesh(
+#    (128.0/2.0, 64.0/2.0, 180/2.0), (30, 15, 36),
+#    d = 4,
+#    domains = { 1: FM1(), 2: NM(), 3: FM2()}
+#  )
+
+mesher = Mesher()
+mesher.create_cuboid((128.0/2.0, 64.0/2.0, 180/2.0), (30, 15, 36))
+mesher.create_celldomain(FM1(), 1)
+mesher.create_celldomain(NM(),  2)
+mesher.create_celldomain(FM2(), 3)
+mesh = mesher.mesh()
 
 #######################################
 #### DEFINE STATE AND MATERIAL
 #######################################
 
 state = State(mesh, {'magnetic': (1, 3), 'conducting': (1, 2, 3)},
-    m = Constant((0, 0, 1)),
+    m = Constant((1, 0, 0)),
     s = Constant((0, 0, 0)),
     j = Constant((0, 0, 1e11))
   )
@@ -37,7 +44,7 @@ state.material['magnetic'] = Material(
   alpha      = 0.1,
   ms         = 8e5,
   Aex        = 13e-12,
-  K_uni      = 500,
+  K_uni      = 5e2,
   K_uni_axis = (0, 0, 1),
   D0         = 1e-3,
   beta       = 0.9,
@@ -56,8 +63,8 @@ state.material['!magnetic']  = Material(
 )
 
 external_field = ExternalField((0.0, 0.0, 0.0))
-llg      = LLG([
-  DemagField(sample_size, 2),
+llg      = LLGAlougesProject([
+  DemagFieldFK(),
   UniaxialAnisotropyField(),
   SpinCurrent(),
   external_field
@@ -73,9 +80,16 @@ for i in range(steps+1):
   external_field.set((0.0, 0.0, (1.0 - 2.0*i/steps) * 60e-3/Constants.mu0))
 
   # relax
-  for j in range(500):
+  for j in range(1000):
     llg.step(state, 1e-12)
     spindiff.step(state, 1e-12)
+
+    #if j % 10 == 0:
+    #  f = File("data/m_%d.pvd" % (j / 10))
+    #  f << state.m
+
+    #  f = File("data/s_%d.pvd" % (j / 10))
+    #  f << state.s
 
   m_x = assemble(state.m[0] / v_magnetic * state.dx('magnetic'))
   m_y = assemble(state.m[1] / v_magnetic * state.dx('magnetic'))
