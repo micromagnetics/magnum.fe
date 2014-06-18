b42a1371e408a5531b7680291674c50a6ed98414
Claas Abert <claas@cabird.de>
2014-06-18 14:55:01 +0200

    rename and fix implicit llg integrator and add tests

--- tests/llg_test.py
+++ tests/llg_test.py
@@ -6,10 +6,12 @@ import os
 set_log_active(False)
 
 mesh, sample_size = DemagField.create_mesh((50.0/2.0, 50.0/2.0, 3.0/2.0), (49, 49, 1), d=3)
+V        = VectorFunctionSpace(mesh, "CG", 1)
+ref      = Function(V, os.path.dirname(os.path.realpath(__file__)) + "/ref/llg_v.xml")
 volume   = 50.0 * 50.0 * 3.0
 arg      = "sqrt((3.141592*(x[0]/1e1))*(3.141592*(x[0]/1e1)))"
 m_expr   = Expression(("cos(%s)" % arg, "sin(%s)" % arg, "0.0"))
-ref_file = os.path.dirname(os.path.realpath(__file__)) + "/ref/llg_v.xml"
+demag    = DemagField("ST", sample_size, 1)
 
 class LlgTest(unittest.TestCase):
 
@@ -17,25 +19,31 @@ class LlgTest(unittest.TestCase):
     parameters["linear_algebra_backend"] = "Epetra"
     state = State(mesh, material = Material.py(), m = m_expr)
 
-    llg = LLGAlougesLagrange([], scale = 1e-9)
+    llg = LLGAlougesLagrange([demag], scale = 1e-9)
     v   = llg.calculate_v(state, 1e-12)
 
-    ref = Function(state.VectorFunctionSpace(), ref_file)
-
-    error = assemble(inner(ref - v, ref - v) / inner(ref, ref) * state.dx('magnetic')) / volume
-    self.assertTrue(error < 0.1)
+    error = assemble(inner(ref - v, ref - v) / inner(ref, ref) * state.dx('magnetic')) / volume * 1e-12
+    self.assertTrue(error < 1e-13)
 
   def test_llg_alouges_project(self):
     parameters["linear_algebra_backend"] = "PETSc"
     state = State(mesh, material = Material.py(), m = m_expr)
 
-    llg = LLGAlougesProject([], scale = 1e-9)
+    llg = LLGAlougesProject([demag], scale = 1e-9)
     v   = llg.calculate_v(state, 1e-12)
 
-    ref = Function(state.VectorFunctionSpace(), ref_file)
+    error = assemble(inner(ref - v, ref - v) / inner(ref, ref) * state.dx('magnetic')) / volume * 1e-12
+    self.assertTrue(error < 1e-13)
 
-    error = assemble(inner(ref - v, ref - v) / inner(ref, ref) * state.dx('magnetic')) / volume
-    self.assertTrue(error < 0.1)
+  def test_llg_alouges_full_implicit(self):
+    parameters["linear_algebra_backend"] = "Epetra"
+    state = State(mesh.with_shell, material = Material.py(), m = m_expr)
+
+    llg = LLGAlougesFullImplicit([], sample_size, scale = 1e-9, demag_order = 1)
+    v   = llg.calculate_v(state, 1e-12)
+
+    error = assemble(inner(ref - v, ref - v) / inner(ref, ref) * dx(mesh)) / volume * 1e-12
+    self.assertTrue(error < 1e-8)
 
 if __name__ == '__main__':
     unittest.main()
